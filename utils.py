b6b766e82057d73e4911bc3ece00377c3f37841b
Juan Camilo Fernandez Prato <jcfernandezp@unal.edu.co>
2019-01-15 15:53:17 -0500

    update video_input
    change also some syntax

--- utils.py
+++ utils.py
@@ -1,13 +1,13 @@
-import numpy as np
 import yaml
+
 
 def yaml_to_dict(yml_path):
 
 	with open(yml_path, 'r') as stream:
-
-	    try:
-	      params = yaml.load(stream)
-	    except yaml.YAMLError as exc:
-	      print(exc)
+		try:
+			params = yaml.load(stream)
+		except yaml.YAMLError as exc:
+			params = None
+			print(exc)
 
 	return params
--- video_input.py
+++ video_input.py
@@ -2,26 +2,33 @@ import cv2
 import os
 import random
 import numpy as np
-from IPython import display
+
 
 # Load video and yield frames
-def load_video(path, max_frames=15, resize=(224, 224)):
-
+def load_video(path, max_frames=0, resize=(224, 224), skip_frames=None):
     cap = cv2.VideoCapture(path)
     frames = []
+    count_frames = 0
     try:
         while True:
             ret, frame = cap.read()
             if not ret:
                 break
-            frame = cv2.resize(frame, resize)
-            frame = frame[:, :, [2, 1, 0]]
-            frames.append(frame)
+            if skip_frames:
+                if count_frames % skip_frames == 0:
+                    frame = cv2.resize(frame, resize)
+                    frame = frame[:, :, [2, 1, 0]]
+                    frames.append(frame)
+            else:
+                frame = cv2.resize(frame, resize)
+                frame = frame[:, :, [2, 1, 0]]
+                frames.append(frame)
 
             if len(frames) == max_frames:
                 yield np.array(frames) / 255.0
                 frames = []
-                continue
+
+            count_frames += 1
     finally:
         cap.release()
     yield np.array(frames) / 255.0
@@ -30,6 +37,7 @@ def load_video(path, max_frames=15, resize=(224, 224)):
 def all_data_videos(params):
     
     list_videos = os.listdir(params['videos_folder'])
+    a = None
 
     if params['shuffle']:
         random.shuffle(list_videos)
@@ -38,12 +46,3 @@ def all_data_videos(params):
         a = load_video(video)
 
     return a
-
-
-# Generate GIF from video frames
-def animate(images):
-    converted_images = np.clip(images * 255, 0, 255).astype(np.uint8)
-    imageio.mimsave('./animation.gif', converted_images, fps=30)
-    with open('./animation.gif', 'rb') as f:
-        display.display(display.Image(data=f.read(), height=300))
-
