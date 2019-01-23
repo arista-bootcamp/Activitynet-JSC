d78a4af503c47a84adbe6becf7a0d93ade872d37
Juan Camilo Fernandez Prato <jcfernandezp@unal.edu.co>
2019-01-23 18:29:03 -0500

    added loss function and metrics by frame

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
+import numpy as np
 import tensorflow as tf
-from keras.applications.inception_resnet_v2 import InceptionResNetV2
 from keras.models import Model
+from keras.applications.inception_resnet_v2 import InceptionResNetV2
 
 
 def _initialize_pretrained_model(base_model_layer='conv_7b'):
@@ -10,6 +11,7 @@ def _initialize_pretrained_model(base_model_layer='conv_7b'):
 
 
 def _add_fc_layer(inputs, training):
+    """
     # First FC Layer
     model = tf.layers.dense(inputs=inputs, units=4096)
     model = tf.layers.batch_normalization(model, training=training)
@@ -19,9 +21,9 @@ def _add_fc_layer(inputs, training):
     model = tf.layers.dense(inputs=model, units=4096)
     model = tf.layers.batch_normalization(model, training=training)
     model = tf.nn.relu(model)
-
+    """
     # Third FC Layer
-    model = tf.layers.dense(inputs=model, units=1000)
+    model = tf.layers.dense(inputs=inputs, units=1000)
     model = tf.layers.batch_normalization(model, training=training)
     model = tf.nn.relu(model)
 
@@ -47,6 +49,12 @@ def _add_regular_rnn_layers(inputs, params):
     return output
 
 
+def _unique_tf(vol):
+    y, idx, count = tf.unique_with_counts(vol)
+    mode = y[tf.argmax(count)]
+    return mode
+
+
 def model_fn(features, mode, params):
     inputs = features['frames_batch']
     labels = features['labels_batch']
@@ -60,16 +68,30 @@ def model_fn(features, mode, params):
     logits = _add_regular_rnn_layers(fc_layers, params)
     print('*******LOGITS.SHAPE Before FC*******', logits.shape)
 
-    y_pred = tf.argmax(input=logits, axis=1)
+    probabilities = tf.nn.softmax(logits, axis=2, name="softmax_tensor")
+
+    if params['predict_mode'] == 'last':
+        pass
+    elif params['predict_mode'] == 'mode':
+        pass
+
+    y_pred = tf.map_fn(_unique_tf, tf.argmax(probabilities, axis=2))
+
     predictions = {
         "classes": y_pred,
-        "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
+        "probabilities": probabilities
     }
 
     if mode == tf.estimator.ModeKeys.PREDICT:
         return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
 
-    loss = tf.losses.softmax_cross_entropy(labels=labels, predictions=logits)
+    loss = tf.losses.softmax_cross_entropy(onehot_labels=labels, logits=logits)
+
+    precision = tf.metrics.precision_at_thresholds(labels=labels,
+                                                   predictions=probabilities,
+                                                   thresholds=list(np.linspace(2 / 101, 10 / 101, 5)))
+
+    mean_precision = tf.metrics.mean(precision)
 
     if mode == tf.estimator.ModeKeys.TRAIN:
         update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
@@ -78,12 +100,10 @@ def model_fn(features, mode, params):
             optimizer = tf.train.AdamOptimizer(learning_rate=params['learning_rate'])
             train_op = optimizer.minimize(loss=loss, global_step=tf.train.get_global_step())
 
-        tf.summary.scalar('total_loss', tf.losses.get_total_loss())
-        tf.summary.scalar('accuracy', tf.metrics.accuracy(tf.argmax(input=labels, axis=1), y_pred)[1])
+        tf.summary.scalar('mean_precision_train', tf.math.reduce_mean(precision[1]))
 
         return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
 
-    eval_metric_ops = {"accuracy": tf.metrics.accuracy(labels=tf.argmax(input=labels, axis=1),
-                                                       predictions=predictions["classes"])}
+    eval_metric_ops = {'precision': precision, 'mean_precision_eval': mean_precision}
 
     return tf.estimator.EstimatorSpec(mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
